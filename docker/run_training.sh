33e02d7dbbdb1237dc4bbdf5038e5b9ee8536b44
AmelusVermin <marvin.meusel@web.de>
2022-02-18 18:54:29 +0100

    fixed run_training.sh for docker image

--- docker/run_training.sh
+++ docker/run_training.sh
@@ -8,14 +8,13 @@ do
         esac
 done
 
-source /.bashrc
-source /catkin_ws/devel/setup.bash 
-source /.profile
-source `which virtualenvwrapper.sh`
+git pull
+cd /
+source /root/.bashrc
+source catkin_ws/devel/setup.bash 
 
 roslaunch arena_bringup start_training_2.launch train_mode:=true num_envs:=$num_env map_file:=random_map show_rviz:=false &>/dev/null &
 sleep 5
 
-workon rosnav
 roscd arena_combined_planner_drl/ 
 python3 scripts/train.py -cf $config_path
